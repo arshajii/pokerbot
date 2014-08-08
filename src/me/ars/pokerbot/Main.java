d6df1b2b8f3789b7e9e24727a1b1fd72104579b3
A. R. Shajii <ariya.r.shajii@gmail.com>
2014-08-08 10:11:57 -0400

    Move parameters to external config file
    Also add options for channel port and server password.

--- src/me/ars/pokerbot/Main.java
+++ src/me/ars/pokerbot/Main.java
@@ -10,7 +10,12 @@ public class Main {
 		bot.setVerbose(Constants.VERBOSE);
 
 		try {
-			bot.connect(Constants.HOST);
+			if (Constants.SERVER_PASS == null) {
+				bot.connect(Constants.HOST, Constants.PORT);
+			} else {
+				bot.connect(Constants.HOST, Constants.PORT,
+						Constants.SERVER_PASS);
+			}
 			bot.joinGameChannel(Constants.CHANNEL_KEY);
 		} catch (IrcException | IOException e) {
 			e.printStackTrace();
